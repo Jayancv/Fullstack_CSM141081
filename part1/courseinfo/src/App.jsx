8392ce0b3a6981a0eecaf0494b4e6d3f832e40ba
Jayancv <jcvidanapathirana@gmail.com>
2024-08-22 23:48:25 +0300

    Exercises-1.1-1.2 added

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+
+function Header(props) {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  );
+};
+
+function Content(props) {
+  console.log(props)
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercise} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercise} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercise} />
+    </div>
+  );
+}
+
+
+function Total(props) {
+  return (
+    <p>
+      Number of exercises{" "}{props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}
+    </p>
+  );
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [{ name: part1, exercise: exercises1 }, { name: part2, exercise: exercises2 }, { name: part3, exercise: exercises3 }]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
