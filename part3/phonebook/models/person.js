23cee17b79e76af9f67e3e387bec01a33f3c2321
Jayancv <jcvidanapathirana@gmail.com>
2025-06-17 00:29:27 +0300

    Exercises-3.15-3.18 added

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -19,13 +19,13 @@ mongoose.connect(url).then(() => {
 
 
 const personSchema = new mongoose.Schema({
-  id: String,
   name: String,
   number: String,
 });
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id;
         delete returnedObject.__v;
     }
