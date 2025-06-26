9dea3b0161c8f9744656b94ae0428dfd87cbfe70
Jayancv <jcvidanapathirana@gmail.com>
2025-06-26 14:42:06 +0300

    Update country service

--- part7/country-hook/src/services/country.js
+++ part7/country-hook/src/services/country.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-const getCountries = () => {
-  const request = axios.get('https://studies.cs.helsinki.fi/restcountries/api/all');
-  return request.then((response) => response.data);
-};
-
 const getCountry = (country) => {
   const request = axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`);
   return request
     .then((response) => response.data)
     .catch((error) => console.error(error.message));
 };
-export default { getCountries, getCountry };
+export default { getCountry };
